252103bc804c5a3fa97d5c8a5d054a44e30b9111
cliccato <cliccato@bigdi.cc>
2022-10-16 13:39:24 +0200

    Correct bugs and add some feature

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+install:
+	sudo mv bin/xsh $PATH
+
 compile:
 	gcc src/*.c -o bin/xsh --no-warnings
 
--- src/main.c
+++ src/main.c
@@ -12,101 +12,82 @@ int executeCmd(char **params);
 #define MAX_COMMAND_LENGTH 100
 #define MAX_NUMBER_OF_PARAMS 10
 #define HOST_NAME_MAX 20
+#define MAX_BUF 200
 
 int main() {
-  char cmd[MAX_COMMAND_LENGTH + 1];
-  char *params[MAX_NUMBER_OF_PARAMS + 1];
-  char hostname[HOST_NAME_MAX + 1];
-  int cmdNumber = 0;
-  int showRow = 0;
+    //char path[MAX_BUF];
+    char cmd[MAX_COMMAND_LENGTH + 1];
+    char *params[MAX_NUMBER_OF_PARAMS + 1];
+    char hostname[HOST_NAME_MAX + 1];
+    int lines = 0;
+    int showLines = 0;
+    int ok;
 
-  // clear();
+    //clear();
 
-  while (1) {
-    // Print command prompt
-    char *username = getenv("USER");
-    gethostname(hostname, HOST_NAME_MAX + 1);
+    while (ok) {
+        char *username = getenv("USER");
+        gethostname(hostname, HOST_NAME_MAX + 1);
+	//getcwd(path, MAX_BUF);
 
-    cmdNumber++;
+        lines++;
 
-    if (showRow)
-      printf("%d\t%s@%s$ ", cmdNumber, username, hostname);
-    else
-      printf("%s@%s$ ", username, hostname);
+        if (showLines)
+            printf("%d\t%s@%s$ ", lines, username, hostname);
+        else
+            printf("%s@%s$ ", username, hostname);
+        
+        if (fgets(cmd, sizeof(cmd), stdin) == NULL)
+            break;
 
-    // Read command from standard input, exit on Ctrl+D
-    if (fgets(cmd, sizeof(cmd), stdin) == NULL)
-      break;
+        if (cmd[strlen(cmd) - 1] == '\n') {
+            cmd[strlen(cmd) - 1] = '\0';
+        }
 
-    // Remove trailing newline character, if any
-    if (cmd[strlen(cmd) - 1] == '\n') {
-      cmd[strlen(cmd) - 1] = '\0';
-    }
+        parseCmd(cmd, params);
 
-    // Split cmd into array of parameters
-    parseCmd(cmd, params);
+        if (strcmp(params[0], "exit") == 0)
+            exit(0);
+        
+	if (strcmp(params[0], "cd") == 0) {
+	    chdir(params[1]);
+	    //strcpy(path, params[1]);
+	    continue;
+	}
 
-    // Exit?
-    if (strcmp(params[0], "exit") == 0)
-      break;
+        if (strcmp(params[0], "xsh") == 0) {
+            if (strcmp(params[1], "zero") == 0) {
+                lines = 0;
+                continue;
+            }
+        
+            if (strcmp(params[1], "change") == 0) {
+                lines = 0;
+                showLines = showLines ? 0 : 1;
+                continue;
+            }
+        }
 
-    if (strcmp(params[0], "xsh") == 0) {
-      if (strcmp(params[1], "zero") == 0) {
-        cmdNumber = 0;
-        continue;
-      }
-  
-      if (strcmp(params[1], "change") == 0) {
-        cmdNumber = 0;
-        showRow = showRow ? 0 : 1;
-        continue;
-      }
-    }
-
-    // Execute command
-    if (executeCmd(params) == 0)
-      continue;
+    
+        ok = (executeCmd(params) == -1) ? 0 : 1;
   }
 
   return 0;
 }
 
-// Split cmd into array of parameters
 void parseCmd(char *cmd, char **params) {
-  for (int i = 0; i < MAX_NUMBER_OF_PARAMS; i++) {
-    params[i] = strsep(&cmd, " ");
-    if (params[i] == NULL)
-      break;
-  }
+    for (int i = 0; i < MAX_NUMBER_OF_PARAMS; i++) {
+        params[i] = strsep(&cmd, " ");
+        if (params[i] == NULL)
+            break;
+    }
 }
 
 int executeCmd(char **params) {
-  // Fork process
-  pid_t pid = fork();
+    int code = execvp(params[0], params);
 
-  // Error
-  if (pid == -1) {
-    char *error = strerror(errno);
-    printf("fork: %s\n", error);
-    return 1;
-  }
+    if (code == -1)
+        printf("%s: error", params[0]);
 
-  // Child process
-  else if (pid == 0) {
-    // Execute command
-    execvp(params[0], params);
-
-    // Error occurred
-    char *error = strerror(errno);
-    printf("xsh: %s: %s\n", params[0], error);
-    return 0;
-  }
-
-  // Parent process
-  else {
-    // Wait for child process to finish
-    int childStatus;
-    waitpid(pid, &childStatus, 0);
-    return 1;
-  }
+    return code;
 }
