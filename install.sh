06897e4fbfaa53e825fea70f0d7ebfe0e33f6cca
cliccato <cliccato@bigdi.cc>
2022-10-19 21:26:51 +0200

    Added help message, version and created some commands

--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo cp bin/xsh $PATH
--- src/main.c
+++ src/main.c
@@ -4,76 +4,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
 
-void parseCmd(char *cmd, char **params);
-int executeCmd(char **params);
-
-#define clear() printf("\e[1;1H\e[2J")
 #define MAX_COMMAND_LENGTH 100
 #define MAX_NUMBER_OF_PARAMS 10
 #define HOST_NAME_MAX 20
-#define MAX_BUF 200
+#define PATH_MAX 200
+#define clear() printf("\e[1;1H\e[2J")
 
-int main() {
-    //char path[MAX_BUF];
-    char cmd[MAX_COMMAND_LENGTH + 1];
-    char *params[MAX_NUMBER_OF_PARAMS + 1];
-    char hostname[HOST_NAME_MAX + 1];
-    int lines = 0;
-    int showLines = 0;
-    int ok;
-
-    //clear();
-
-    while (ok) {
-        char *username = getenv("USER");
-        gethostname(hostname, HOST_NAME_MAX + 1);
-	//getcwd(path, MAX_BUF);
-
-        lines++;
-
-        if (showLines)
-            printf("%d\t%s@%s$ ", lines, username, hostname);
-        else
-            printf("%s@%s$ ", username, hostname);
-        
-        if (fgets(cmd, sizeof(cmd), stdin) == NULL)
-            break;
-
-        if (cmd[strlen(cmd) - 1] == '\n') {
-            cmd[strlen(cmd) - 1] = '\0';
-        }
-
-        parseCmd(cmd, params);
-
-        if (strcmp(params[0], "exit") == 0)
-            exit(0);
-        
-	if (strcmp(params[0], "cd") == 0) {
-	    chdir(params[1]);
-	    //strcpy(path, params[1]);
-	    continue;
-	}
-
-        if (strcmp(params[0], "xsh") == 0) {
-            if (strcmp(params[1], "zero") == 0) {
-                lines = 0;
-                continue;
-            }
-        
-            if (strcmp(params[1], "change") == 0) {
-                lines = 0;
-                showLines = showLines ? 0 : 1;
-                continue;
-            }
-        }
-
-    
-        ok = (executeCmd(params) == -1) ? 0 : 1;
-  }
-
-  return 0;
-}
+#define VERSION "1.1.0"
 
 void parseCmd(char *cmd, char **params) {
     for (int i = 0; i < MAX_NUMBER_OF_PARAMS; i++) {
@@ -84,10 +24,88 @@ void parseCmd(char *cmd, char **params) {
 }
 
 int executeCmd(char **params) {
-    int code = execvp(params[0], params);
+    pid_t pid = fork();
 
-    if (code == -1)
-        printf("%s: error", params[0]);
+    if (pid == -1) { //ForkCreationError
+        char *error = strerror(errno);
+        printf("fork: %s\n", error);
+        return 1;
+    }
 
-    return code;
+    if (pid == 0) {
+        //Execution
+	execvp(params[0], params);
+
+	//Error
+        char *error = strerror(errno);
+        printf("xsh: %s: %s\n", params[0], error);
+
+	//End Execution
+        kill(getpid(), SIGKILL);
+        return 0;
+
+    } else {
+        int childStatus;
+        waitpid(pid, &childStatus, 0);
+        return 1;
+    }
+}
+
+void main(int argc, char *argv[]) {
+    if (argc > 1) {
+        if (strcmp(argv[1], "-h")==0 || strcmp(argv[1], "--help")==0) {
+	    printf("Help message\n");
+	    exit(0);
+
+        } else if (strcmp(argv[1], "-v")==0 || strcmp(argv[1], "--version")==0) {
+            printf(VERSION);
+	    printf("\n");
+            exit(0);
+        }
+    }
+
+    char path[PATH_MAX];
+    char cmd[MAX_COMMAND_LENGTH + 1];
+    char *params[MAX_NUMBER_OF_PARAMS + 1];
+    char hostname[HOST_NAME_MAX + 1];
+    int line = 0;
+
+    clear();
+    printf("xsh shell\n\n");
+
+    while(1) {
+        char *username = getenv("USER");
+        gethostname(hostname, HOST_NAME_MAX + 1);
+	getcwd(path, PATH_MAX);
+
+        line++;
+        printf("%s@%s$ ", username, hostname);
+        
+        if (fgets(cmd, sizeof(cmd), stdin) == NULL)
+            break;
+
+        if (cmd[strlen(cmd) - 1] == '\n')
+            cmd[strlen(cmd) - 1] = '\0';
+
+        parseCmd(cmd, params);
+
+        if (strcmp(params[0], "exit") == 0)
+            exit(1);
+        
+	if (strcmp(params[0], "cd") == 0) {
+	    chdir(params[1]);
+	    strcpy(path, params[1]);
+	    continue;
+	}
+
+	if (strcmp(params[0], "line") == 0) {
+	    printf("%d\n", line);
+	    continue;
+	}
+
+	if (!executeCmd(params))
+	    break;
+    
+    }
+
 }
